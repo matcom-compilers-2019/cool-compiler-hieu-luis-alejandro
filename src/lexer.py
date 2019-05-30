40a5c52e7e1732cef865053ef7c6812c3475e908
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-05-30 18:19:31 -0400

    Fix Cool keywords

--- src/lexer.py
+++ src/lexer.py
@@ -4,110 +4,44 @@ from ply.lex import TOKEN
 
 class CoolLexer:
 
-	
-	# COOL FEATURES DEFINITIONS
-    @property
-    def tokens_collection(self):
-        """
-        Collection of COOL Syntax Tokens.
-        :return: Tuple.
-        """
-        return (
-            # Identifiers
-            "ID", "TYPE",
+	# ############################## LEXER DEFINITIONS	######################################
 
-            # Primitive Types
-            "INTEGER", "STRING", "BOOLEAN",
+	#	 Collection of COOL Syntax Tokens.
+	token_names = (
+		# Identifiers
+		"ID", "TYPE",
 
-            # Literals
-            "LPAREN", "RPAREN", "LBRACE", "RBRACE", "COLON", "COMMA", "DOT", "SEMICOLON", "AT",
+		# Primitive Types
+		"INTEGER", "STRING", "BOOLEAN",
 
-            # Operators
-            "PLUS", "MINUS", "MULTIPLY", "DIVIDE", "EQ", "LT", "LTEQ", "ASSIGN", "INT_COMP", "NOT",
+		# Literals
+		"LPAREN", "RPAREN", "LBRACE", "RBRACE", "COLON", "COMMA", "DOT", "SEMICOLON", "AT",
 
-            # Special Operators
-            "ARROW"
-        )
+		# Operators
+		"PLUS", "MINUS", "MULTIPLY", "DIVIDE", "EQ", "LT", "LTEQ", "ASSIGN", "INT_COMP", "NOT",
 
-    @property
-    def basic_reserved(self):
-        """
-        Map of Basic-COOL reserved keywords.
-        :return: dict.
-        """
-        return {
-            "case": "CASE",
-            "class": "CLASS",
-            "else": "ELSE",
-            "esac": "ESAC",
-            "fi": "FI",
-            "if": "IF",
-            "in": "IN",
-            "inherits": "INHERITS",
-            "isvoid": "ISVOID",
-            "let": "LET",
-            "loop": "LOOP",
-            "new": "NEW",
-            "of": "OF",
-            "pool": "POOL",
-            "self": "SELF",
-            "then": "THEN",
-            "while": "WHILE"
-        }
+		# Special Operators
+		"ARROW"
+	)
 
-    @property
-    def extended_reserved(self):
-        """
-        Map of Extended-COOL reserved keywords.
-        :return: dict.
-        """
-        return {
-            "abstract": "ABSTRACT",
-            "catch": "CATCH",
-            "do": "DO",
-            "def": "DEF",
-            "final": "FINAL",
-            "finally": "FINALLY",
-            "for": "FOR",
-            "forSome": "FORSOME",
-            "explicit": "IMPLICIT",
-            "implicit": "IMPORT",
-            "lazy": "LAZY",
-            "match": "MATCH",
-            "native": "NATIVE",
-            "null": "NULL",
-            "object": "OBJECT",
-            "override": "OVERRIDE",
-            "package": "PACKAGE",
-            "private": "PRIVATE",
-            "protected": "PROTECTED",
-            "requires": "REQUIRES",
-            "return": "RETURN",
-            "sealed": "SEALED",
-            "super": "SUPER",
-            "this": "THIS",
-            "throw": "THROW",
-            "trait": "TRAIT",
-            "try": "TRY",
-            "type": "TYPE",
-            "val": "VAL",
-            "var": "VAR",
-            "with": "WITH",
-            "yield": "YIELD"
-        }
-
-    @property
-    def builtin_types(self):
-        """
-        A map of the built-in types.
-        :return dict
-        """
-        return {
-            "Bool": "BOOL_TYPE",
-            "Int": "INT_TYPE",
-            "IO": "IO_TYPE",
-            "Main": "MAIN_TYPE",
-            "Object": "OBJECT_TYPE",
-            "String": "STRING_TYPE",
-            "SELF_TYPE": "SELF_TYPE"
-        }
+	#	 Map of COOL reserved keywords.
+	cool_reserved = {
+		"case": "CASE",
+		"class": "CLASS",
+		"else": "ELSE",
+		"esac": "ESAC",
+		"fi": "FI",
+		"if": "IF",
+		"in": "IN",
+		"inherits": "INHERITS",
+		"isvoid": "ISVOID",
+		"let": "LET",
+		"loop": "LOOP",
+		"new": "NEW",
+		"of": "OF",
+		"pool": "POOL",
+		"self": "SELF",
+		"then": "THEN",
+		"while": "WHILE",
+		"not": "NOT"
+	}
